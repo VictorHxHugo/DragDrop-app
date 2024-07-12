7d6cfee0be9ca350bef4a719f270dc1340b430b0
VictorHxHugo <victorzk6@hotmail.com>
2024-07-11 21:14:58 -0300

    Commit inicial

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importeNode = document.importNode(this.templateElement.content, true);
+    this.element = importeNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
